ebcbed59d6df6734ded93221502e7a4fdd808d2f
Darns <59196040+asa-darlene@users.noreply.github.com>
2020-11-11 21:52:29 +0700

    update Customer
    CustomerModel, CustomerRepository and CustomerController

---
+++ src/main/java/com/example/bookshop/model/Customer.java
@@ -0,0 +1,41 @@
+package com.example.bookshop.model;
+
+import javax.persistence.*;
+
+@Entity(name = "customers")
+public class Customer {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Integer id;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "tel")
+    private String tel;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getTel() {
+        return tel;
+    }
+
+    public void setTel(String tel) {
+        this.tel = tel;
+    }
+}
---
+++ src/main/java/com/example/bookshop/repository/CustomerRepository.java
@@ -0,0 +1,38 @@
+package com.example.bookshop.repository;
+
+import com.example.bookshop.model.Customer;
+import org.springframework.stereotype.Repository;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.Query;
+import javax.transaction.Transactional;
+import java.util.List;
+
+@Repository
+public class CustomerRepository {
+    @PersistenceContext
+    private EntityManager entityManager;
+
+    public List<Customer> showAll(){
+        Query query = entityManager.createQuery("from customers");
+        return query.getResultList();
+    }
+
+    @Transactional
+    public Customer save(Customer customer){
+        entityManager.persist(customer);
+        return customer;
+    }
+
+    public Customer findById(Integer id){
+        return entityManager.find(Customer.class , id);
+    }
+
+    @Transactional
+    public Customer deleteCustomer(Integer id){
+        Customer customer = entityManager.find(Customer.class ,id);
+        entityManager.remove(customer);
+        return customer;
+    }
+}
