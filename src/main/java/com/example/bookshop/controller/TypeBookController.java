faf7f982d14f6d81027bd8b90aafb6d2ab7175ca
Wsasithron <w.sasithorn@kkumail.com>
2020-11-11 21:02:26 +0700

    add delete to controller

--- src/main/java/com/example/bookshop/controller/TypeBookController.java
+++ src/main/java/com/example/bookshop/controller/TypeBookController.java
@@ -3,12 +3,17 @@ package com.example.bookshop.controller;
 import com.example.bookshop.model.TypeBook;
 import com.example.bookshop.model.TypeBookService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
 @RestController
+@RequestMapping("/api/book")
 public class TypeBookController {
     @Autowired
     private TypeBookService typeBookService;
@@ -17,4 +22,12 @@ public class TypeBookController {
     public List<TypeBook> list() {
         return typeBookService.listAll();
     }
+    
+    @RequestMapping(value = "/delete/{id}", method = RequestMethod.DELETE)
+    public String delete(@PathVariable("id") int id) {
+    	this.typeBookService.delete(id);
+    	return "Deleted.";
+   }
+    
+    
 }
