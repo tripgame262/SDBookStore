2b62bc1c0271ef178b3a3888138a07c659e99550
Prik_FD <47244124+tripgame262@users.noreply.github.com>
2020-11-15 20:44:17 +0700

    update database

--- src/main/java/com/example/bookshop/controller/CustomerController.java
+++ src/main/java/com/example/bookshop/controller/CustomerController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/customers")
+@RequestMapping("/api/customer")
 public class CustomerController {
     @Autowired
     private CustomerRepository repository;
