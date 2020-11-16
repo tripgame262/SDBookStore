c9d636522e7fe7b7c73246206acb5f6b3e1626b5
Wsasithron <w.sasithorn@kkumail.com>
2020-11-16 16:14:22 +0700

    o_rder update

--- src/main/java/com/example/bookshop/controller/OrderController.java
+++ src/main/java/com/example/bookshop/controller/OrderController.java
@@ -1,9 +1,14 @@
 package com.example.bookshop.controller;
 
 
+import com.example.bookshop.model.Book;
+import com.example.bookshop.model.Customer;
 import com.example.bookshop.model.O_rder;
 
 
+import com.example.bookshop.model.TypeBook;
+import com.example.bookshop.repository.BookRepository;
+import com.example.bookshop.repository.CustomerRepository;
 import com.example.bookshop.repository.OrderRepository;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -17,6 +22,10 @@ import java.util.List;
 public class OrderController {
     @Autowired
     private OrderRepository Orderrepository;
+    @Autowired
+    private CustomerRepository CustomerRepository;
+    @Autowired
+    private BookRepository BookRepository;
 
     @GetMapping("/showall")
     public List<O_rder> showAll(){
--- src/main/java/com/example/bookshop/repository/CustomerRepository.java
+++ src/main/java/com/example/bookshop/repository/CustomerRepository.java
@@ -35,4 +35,6 @@ public class CustomerRepository {
         entityManager.remove(customer);
         return customer;
     }
+
+
 }
