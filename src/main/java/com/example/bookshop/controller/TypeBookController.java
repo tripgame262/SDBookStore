dc86f8f4f850a1df77cb9aeec186b6c532bcf2de
Prik_FD <47244124+tripgame262@users.noreply.github.com>
2020-11-17 11:15:49 +0700

    Update TypeBookController.java

--- src/main/java/com/example/bookshop/controller/TypeBookController.java
+++ src/main/java/com/example/bookshop/controller/TypeBookController.java
@@ -13,7 +13,7 @@ public class TypeBookController {
     @Autowired
     private TypeBookRepository typeBookRepository;
 
-    @DeleteMapping("/delete/{id}")
+    @GetMapping("/delete/{id}")
     public void delete(@PathVariable("id") Integer id){
         typeBookRepository.deleteTypeBook(id);
     }
